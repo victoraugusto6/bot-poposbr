a19917bf8f136f4b1216ca1a1bc8172265704eee
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-11-22 00:49:48 -0300

    remove reference to heroku

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,7 +4,6 @@ from telegram.ext import CommandHandler, Updater
 
 TELEGRAM_TOKEN = config('TELEGRAM_TOKEN')
 DEBUG = config('DEBUG')
-APP_NAME_HEROKU = config('APP_NAME_HEROKU')
 
 
 def send(msg, chat_id):
@@ -17,7 +16,7 @@ def tutoriais(update, context):
     message += '<strong>Segue a lista de tutoriais:</strong>\n\n'
 
     # Lendo arquivo
-    with open('bot/lista-tutoriais.txt', 'r') as file:
+    with open('lista-tutoriais.txt', 'r') as file:
         message += file.read()
 
     context.bot.send_message(
@@ -43,19 +42,9 @@ def main():
     dispatcher.add_handler(CommandHandler("tutoriais", tutoriais))
     dispatcher.add_handler(CommandHandler("site", site))
 
-    if DEBUG:
-        updater.start_polling()
+    updater.start_polling()
 
-        updater.idle()
-    else:
-        port = config('PORT', cast=int)
-
-        updater.start_webhook(listen="0.0.0.0",
-                              port=port,
-                              url_path=TELEGRAM_TOKEN)
-        updater.bot.setWebhook(f'https://{APP_NAME_HEROKU}.herokuapp.com/{TELEGRAM_TOKEN}')
-
-        updater.idle()
+    updater.idle()
 
 
 if __name__ == "__main__":
